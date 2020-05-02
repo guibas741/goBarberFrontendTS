1ef2f55a7dd329cf3000c70cc0c160ecf78038d3
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-05-02 09:13:13 -0300

    Creates toast component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -12,6 +12,7 @@ const App: React.FC = () => (
       <SignIn />
     </AuthProvider>
 
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
