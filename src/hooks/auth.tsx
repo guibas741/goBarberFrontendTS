7904529e854083f4da0ff255f450b77caca6148c
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-05-23 16:19:47 -0300

    Creates header of dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,9 +1,14 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  avatar_url: string;
+  name: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 interface SignInCredentials {
   email: string;
@@ -11,7 +16,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
