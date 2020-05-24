0ce845b04ec9dee504828ab1d5ca939170ba426d
Guilherme Bublitz <bublitz.stein@gmail.com>
2020-05-24 12:53:04 -0300

    Connects api with calendar to disable days that are not available

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -29,6 +29,7 @@ export const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
 
@@ -44,6 +45,8 @@ export const AuthProvider: React.FC = ({ children }) => {
     const { token, user } = response.data;
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
